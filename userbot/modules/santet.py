25ed9b6f5cec0456b83fdf713ed8d2976dceab92
yusniii <83091198+yusniii@users.noreply.github.com>
2021-10-15 12:07:51 +0800

    Create santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -6,11 +6,11 @@ from userbot.events import register
 @register(outgoing=True, pattern=r'^\.santet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Anda Telah Mengaktifkan Perintah Santet Online ツ`")
+    await typew.edit("`PERINTAH SANTET ONLINE UNTUK ANK HARAM AKTIF`")
     sleep(2)
-    await typew.edit("`Mencari Nama Orang Ini...`")
+    await typew.edit("`Mencari nama binatang ini...`")
     sleep(1)
-    await typew.edit("`Santet Online Segera Dilakukan`")
+    await typew.edit("`Santet Online Otw`")
     sleep(1)
     await typew.edit("0%")
     number = 1
